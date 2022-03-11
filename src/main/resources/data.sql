b19a517efa968ad41bcf7b2a47649c0c613f704f
Jose <77968901+jserranodev@users.noreply.github.com>
2022-03-11 09:15:00 +0100

    Añadido data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into empleado (id, nombre, apellidos)
+ select 1, 'Rocío', 'De la O' from dual where not exists (select 1 from empleado where id = 1);
+
+insert into empleado (id, nombre, apellidos)
+	select 2, 'Alberto', 'Del Monte' from dual where not exists (select 1 from empleado where id = 2);
